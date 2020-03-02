087cc4107a08f42cfd4d80c3e092583284bc073a
Awais Jibran <awaisdar001@gmail.com>
2020-03-02 21:45:17 +0500

    insulate schedules from module_render.

--- openedx/core/djangoapps/schedules/content_highlights.py
+++ openedx/core/djangoapps/schedules/content_highlights.py
@@ -6,8 +6,6 @@ schedule experience built on the Schedules app.
 
 import logging
 
-from lms.djangoapps.courseware.model_data import FieldDataCache
-from lms.djangoapps.courseware.module_render import get_module_for_descriptor
 from openedx.core.djangoapps.schedules.config import COURSE_UPDATE_WAFFLE_FLAG
 from openedx.core.djangoapps.schedules.exceptions import CourseUpdateDoesNotExist
 from openedx.core.lib.request_utils import get_request_or_stub
@@ -93,6 +91,9 @@ def _get_course_descriptor(course_key):
 
 def _get_course_module(course_descriptor, user):
     # Fake a request to fool parts of the courseware that want to inspect it.
+    from lms.djangoapps.courseware.model_data import FieldDataCache
+    from lms.djangoapps.courseware.module_render import get_module_for_descriptor
+
     request = get_request_or_stub()
     request.user = user
 
